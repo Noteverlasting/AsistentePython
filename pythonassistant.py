a074a537bea5b04e0862d7df66948a72b0383bf6
Noteverlasting <ofballester@gmail.com>
2025-06-30 13:06:11 +0200

    subida

--- pythonassistant.py
+++ pythonassistant.py
@@ -4,6 +4,11 @@ import speech_recognition as sr
 import pyttsx3
 import io 
 import datetime
+import pyjokes  # Para chistes
+import pywhatkit  # Para enviar mensajes de WhatsApp
+import webbrowser  # Para abrir páginas web
+
+nombre_asi = "La asombrosa Petunia María, prima lejana de Pepa Pig"
 
 # funcion para que la máquina escuche lo que decimos y lo convierta a texto
 def audio_pc():
@@ -16,7 +21,7 @@ def audio_pc():
         audio = r.listen(source) #escucha el audio del micrófono
         print("Reconociendo...") #mensaje para saber que se está procesando el audio
         try:
-            text = r.recognize_google(audio, Language='es-ES') #reconoce el audio y lo convierte a texto
+            text = r.recognize_google(audio, language='es-ES') #reconoce el audio y lo convierte a texto
             print("Has dicho: " + text)
             return text #retorna el texto reconocido
         except:
@@ -28,7 +33,7 @@ def audio_pc():
 def respuesta_pc(texto):
     try:
         engine = pyttsx3.init()  # Inicializa el motor de texto a voz
-        engine.setProperty('rate', 150)  # Ajusta la velocidad de la voz (150 es un valor común)
+        engine.setProperty('rate', 200)  # Ajusta la velocidad de la voz (150 es un valor común)
         engine.setProperty('volume', 1)  # Ajusta el volumen (0.0 a 1.0)
         engine.say(texto) #Convierte el texto a voz
         engine.runAndWait()  # Espera a que termine de hablar
@@ -55,7 +60,7 @@ def dia_semana():
     }
     respuesta_pc(f"Hoy es {nombres_dias[dia.weekday()]}")  # Responde con el nombre del día de la semana
 
-dia_semana()
+# dia_semana()
 
 def hora_actual():
     #Variable para almacenar la hora actual
@@ -64,4 +69,51 @@ def hora_actual():
     respuesta_pc(mensajehora)
 
 
-hora_actual()
+# hora_actual()
+
+def saludo():
+    hora = datetime.datetime.now()
+    if 6 <= hora.hour < 14:
+        ahora ="Buenos días"
+    elif 14 <= hora.hour < 20:
+        ahora ="Buenas tardes"
+    else:
+        ahora ="Buenas noches"
+
+    respuesta = f"{ahora}, soy {nombre_asi}, tu asistente virtual. ¿En qué puedo ayudarte, puerqui?"
+    respuesta_pc(respuesta)
+
+# saludo()
+
+def centro_de_peticiones():
+    saludo()
+    #bucle infinito hasta que la detengamos para que escuche
+    
+    while True:
+        try:
+            respuesta_pc("Estoy escuchando, puerqui. ¿Qué quieres que haga por ti?")  # Mensaje inicial
+            peticion = audio_pc()
+            print(f"Petición recibida: {peticion}")
+            #Preparamos las peticiones que procesará el asistente
+            if "dime la hora" in peticion:
+                hora_actual()
+                continue
+            elif "qué día es hoy" in peticion:
+                dia_semana()
+                continue
+            elif "eso es todo" in peticion or "fin del programa" in peticion:
+                respuesta_pc("Hasta luego, puerqui")
+                break
+            elif "cuéntame un chiste" in peticion:
+                respuesta_pc(pyjokes.get_joke(language='es', category='all'))
+                continue
+            elif "reproduce" in peticion:
+                respuesta_pc("A tus ordenes puerquita")
+                pywhatkit.playonyt(peticion)
+                continue
+
+        except:
+            respuesta_pc("Lo siento, no he podido entenderte. Por favor, inténtalo de nuevo.")
+
+
+centro_de_peticiones()
